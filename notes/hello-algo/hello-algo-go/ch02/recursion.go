fcb36bac2ad058f7dd95ad09e5ef8573c13b4e0e
Andy Ron <randy.njfu@gmail.com>
2024-07-18 17:57:26 +0800

    hello-algo-go

---
+++ notes/hello-algo/hello-algo-go/ch02/recursion.go
@@ -0,0 +1,35 @@
+package main
+
+// 递归
+func recur(n int) int {
+	// 终止条件
+	if n == 1 {
+		return 1
+	}
+	// 递：递归调用
+	res := recur(n - 1)
+	// 归：返回接结果
+	return n + res
+}
+
+// 尾递归
+func tailRecur(n int, res int) int {
+	// 终止条件
+	if n == 0 {
+		return res
+	}
+	// 尾递归调用
+	return tailRecur(n-1, res+n)
+}
+
+// 斐波那契数列：递归
+func fib(n int) int {
+	// 终止条件
+	if n == 1 || n == 2 {
+		return n - 1
+	}
+	// 递归调用 f(n) = f(n-1) + f(n-2)
+	res := fib(n-1) + fib(n-2)
+	// 返回结果 f(n)
+	return res
+}
