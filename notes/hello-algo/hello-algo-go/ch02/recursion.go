235e2843437abb452cc1b297728c9343e2ac9ade
Andy Ron <randy.njfu@gmail.com>
2024-07-19 23:41:55 +0800

    hello-algo-go

--- notes/hello-algo/hello-algo-go/ch02/recursion.go
+++ notes/hello-algo/hello-algo-go/ch02/recursion.go
@@ -1,5 +1,7 @@
 package main
 
+import "container/list"
+
 // 递归
 func recur(n int) int {
 	// 终止条件
@@ -8,7 +10,7 @@ func recur(n int) int {
 	}
 	// 递：递归调用
 	res := recur(n - 1)
-	// 归：返回接结果
+	// 归：返回结果
 	return n + res
 }
 
@@ -31,5 +33,24 @@ func fib(n int) int {
 	// 递归调用 f(n) = f(n-1) + f(n-2)
 	res := fib(n-1) + fib(n-2)
 	// 返回结果 f(n)
+	return res
+}
+
+// 使用迭代模拟递归
+func forLoopRecur(n int) int {
+	// 使用一个显式的栈来模拟系统调用栈
+	stack := list.New()
+	res := 0
+	// 递：递归调用
+	for i := n; i > 0; i-- {
+		// 通过入栈操作模拟"递"
+		stack.PushBack(i)
+	}
+	// 归：返回结果
+	for stack.Len() != 0 {
+		// 通过出栈操作模拟"归"
+		res += stack.Back().Value.(int)
+		stack.Remove(stack.Back())
+	}
 	return res
 }
---
+++ notes/hello-algo/hello-algo-go/ch02/time_complexity.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func algorithm_A(n int) {
+	fmt.Println(0)
+}
